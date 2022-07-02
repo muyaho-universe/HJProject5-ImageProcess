95cda999dfcb582b4b2061f0a3db5c02503625be
muyaho-universe <kimdaesk@gmail.com>
2022-07-02 14:21:07 +0900

    Working on basic frame

--- app/src/main/java/com/dale/imageprocessor/MainFrame.java
+++ app/src/main/java/com/dale/imageprocessor/MainFrame.java
@@ -6,7 +6,15 @@ public class MainFrame extends JFrame {
 
 	
 	public static void main(String[] args) {
-		
+		MainFrame mainFrame = new MainFrame();
+		mainFrame.run();
+	}
+	
+	private void run() {
+		this.setSize(1280, 720);
+		this.setVisible(true);
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		this.setResizable(false);
 	}
 
 }
---
+++ app/src/main/java/com/dale/imageprocessor/data/MyData.java
@@ -0,0 +1,5 @@
+package com.dale.imageprocessor.data;
+
+public class MyData {
+
+}
